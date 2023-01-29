ee5264a090d2e0000c9f8ee0abc55aa1f30fd2f3
neffej <118024182+neffej@users.noreply.github.com>
2023-01-29 16:54:21 -0500

    made read/write process for express POST method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # express-note-taker
+
+* https://www.techgeeknext.com/nodejs/nodejs-append-data-json-file#:~:text=To%20append%20json%20data%2Felements,a%20string%2C%20and%20save%20it.
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require ('path');
 const db = require('./db/db.json');
 
@@ -12,10 +13,53 @@ app.use(express.json());
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req,res) => res.json(db));
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request receieved to add a note`)
+
+    const { title, text } = req.body;
+
+    if(title && text){
+        const newNote = {
+            title,
+            text,
+        };
+
+        const noteString = JSON.stringify(newNote);
+        const noteHolder = fs.readFileSync('./db/db.json')
+
+        console.log(db.length)
+
+        if (db.length === 0){
+            fs.writeFileSync('./db/db.json', noteString)
+        }else {
+            const json = JSON.parse(noteHolder.toString())
+
+            json.push(newNote);
+            const newJSON = JSON.stringify(json)
+
+            fs.writeFileSync('./db/db.json', newJSON)
+        }
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log (response);
+        res.status(201).json(response);
+    }else{
+        res.status(500).json('Error - please include text and a title in your note');
+    }
+});
 
 app.listen(PORT, ()=> {
     console.log(`App listening at http://localhost:${PORT}`);
