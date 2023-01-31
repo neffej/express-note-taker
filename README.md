bbd55c64c90324fd21e2f5688b048a36d1d95b4f
neffej <118024182+neffej@users.noreply.github.com>
2023-01-31 17:58:22 -0500

    fixed get requests

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # express-note-taker
 
 * https://www.techgeeknext.com/nodejs/nodejs-append-data-json-file#:~:text=To%20append%20json%20data%2Felements,a%20string%2C%20and%20save%20it.
+
+*https://javascript.info/async-await
+
--- server.js
+++ server.js
@@ -20,7 +20,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/notes', (req,res) => res.json(db));
+app.get('/api/notes', (req,res) => 
+    fs.readFile('./db/db.json', 'utf-8',(err, data) => {
+    if (err) {
+        console.error(err);
+    }else{
+        const parsedData = JSON.parse(data);
+    res.json(parsedData);
+    }}
+    ));
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request receieved to add a note`)
@@ -29,39 +37,41 @@ app.post('/api/notes', (req, res) => {
     const id = Math.floor(Math.random()*99)+1
 
     if(title && text){
-        const newNote = {
-            title,
-            text,
-            id,
-        };
+        const newNote = {title, text, id};
 
-        const noteString = JSON.stringify(newNote);
-        const noteHolder = fs.readFileSync('./db/db.json')
+    fs.readFile('./db/db.json', 'utf-8',(err, data) =>{
+        if (err) {
+            console.error(err);
+        }else{
+            // Convert string
+            const parsedNotes = JSON.parse(data);
 
-        console.log(db.length)
+            // Add note
+            parsedNotes.push(newNote)
 
-        if (db.length === 0){
-            fs.writeFileSync('./db/db.json', noteString)
-        }else {
-            const json = JSON.parse(noteHolder.toString())
-
-            json.push(newNote);
-            const newJSON = JSON.stringify(json)
-
-            fs.writeFileSync('./db/db.json', newJSON)
+            // Rewrite file
+            fs.writeFile('./db/db.json',
+            JSON.stringify(parsedNotes, null, 4), 
+            (writeErr) =>
+            writeErr
+            ? console.error(writeErr)
+            : console.log('Successfully updated reviews!')
+            );
         }
+    });
 
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log (response);
-        res.status(201).json(response);
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+    
+    res.status(201).json(response);
     }else{
-        res.status(500).json('Error - please include text and a title in your note');
+        res.status(500).json('Error in saving note');
     }
 });
+
+
 
 app.listen(PORT, ()=> {
     console.log(`App listening at http://localhost:${PORT}`);
