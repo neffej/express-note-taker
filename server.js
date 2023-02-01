0eaf52ba5569caa4ca7decd10c3d9cbcf9d2d4e2
neffej <118024182+neffej@users.noreply.github.com>
2023-02-01 09:48:29 -0500

    updated PORT number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require ('path');
 const db = require('./db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
